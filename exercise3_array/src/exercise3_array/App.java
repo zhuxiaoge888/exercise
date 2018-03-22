98bd0f5a738cc90707af279df0e5db25bd742c3b
zhuxiaoge888 <zhuxiaoge888@gmail.de>
2018-03-22 17:30:28 +0100

    1

--- exercise3_array/src/exercise3_array/App.java
+++ exercise3_array/src/exercise3_array/App.java
@@ -138,9 +138,58 @@ public class App {
 	 * 	To Do!
 	 */
 		
+	/** execrise 23 PrintBinaryBaum
+	 * 	
+	 \
+		BinaryTreeNode root1=new BinaryTreeNode();  
+		BinaryTreeNode node1=new BinaryTreeNode();  
+		BinaryTreeNode node2=new BinaryTreeNode();   
+		BinaryTreeNode node3=new BinaryTreeNode();   
+		BinaryTreeNode node4=new BinaryTreeNode();   
+		BinaryTreeNode node5=new BinaryTreeNode();   
+		BinaryTreeNode node6=new BinaryTreeNode();
+		  
+		root1.left=node1;   root1.right=node2;   
+		node1.left=node3;   node1.right=node4;   node4.left=node5;   
+		node4.right=node6;   root1.value=8;   node1.value=8;   
+		node2.value=7;   node3.value=9;   node4.value=2;   node5.value=4;   node6.value=7;
+		PrintBinaryBaum print = new PrintBinaryBaum();
+		print.print(root1);
+	*/	
 		
 		
+	/** execrise 24 Postordner
+	 * 	
+	
+		BinaryTreeNode root1=new BinaryTreeNode();  
+		BinaryTreeNode node1=new BinaryTreeNode();  
+		BinaryTreeNode node2=new BinaryTreeNode();   
+		BinaryTreeNode node3=new BinaryTreeNode();   
+		BinaryTreeNode node4=new BinaryTreeNode();   
+		BinaryTreeNode node5=new BinaryTreeNode();   
+		BinaryTreeNode node6=new BinaryTreeNode();
+		  
+		root1.left=node1;   root1.right=node2;   
+		node1.left=node3;   node1.right=node4;   node4.left=node5;   
+		node4.right=node6;   root1.value=8;   node1.value=8;   
+		node2.value=7;   node3.value=9;   node4.value=2;   node5.value=4;   node6.value=7;
+		
+		Postordner print = new Postordner();
+		int array[] ={9,4,7,2,8,7,8};
+		boolean check = print.check(root1, array);
+		System.out.println(check);	
+		*/
+		
+	/**execrise 25 PrintPfadInBinaryTree
+	 * 	
+	 */
 		
 		
 	}
+	
+	
+	
+	
+	
+	
 }
--- exercise3_array/src/exercise3_array/BinaryTreeNode.java
+++ exercise3_array/src/exercise3_array/BinaryTreeNode.java
@@ -1,7 +1,24 @@
 package exercise3_array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinaryTreeNode {
 	BinaryTreeNode left;
 	BinaryTreeNode right;
 	int value;
+	
+	public boolean hasleft(){
+		if(this.left!=null) return true;
+		return false;
+		
+	}
+	
+	public boolean hasright(){
+		if(right!=null) return true;
+		return false;
+		
+	}
+	
+	
 }
